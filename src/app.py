c2aea32e785f23c9ec7c81658616dd1898b62f8f
Bob Wang <wangziling1000@gmail.com>
2020-09-12 13:31:04 +0200

    test

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import json
 import os
 import requests
+import random
 
 import boto3
 from aws_lambda_powertools import Logger, Metrics, Tracer
@@ -30,7 +31,17 @@ def handler(event, context):
         raise
 
 def get_page(url, fn=None, save=True):
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
+    headers = get_headers()
     r = requests.get(url, headers=headers)
     print(r.text)
+
+def get_headers():
+    headers = CaseInsensitiveDict()
+    browser_list = [
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .\
+            NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)', ]
+    headers['User-Agent'] = random.choice(browser_list)
+    headers["Accept"] = "application/json"
+    return headers 
